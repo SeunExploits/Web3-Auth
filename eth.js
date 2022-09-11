0dc42cbbce306280e781ec0b249fac8cdc1777f7
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-09-11 17:14:07 +0100

    updated the functionality for bsc also

---
+++ eth.js
@@ -0,0 +1,36 @@
+/* Moralis init code */
+const serverUrl = "https://na9xpnhehmew.usemoralis.com:2053/server";
+const appId = "RRA42KNb8t818QjsYE8qbnOgeeIEGeuSNEVLuUZg";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+        user = await Moralis.authenticate({
+            signingMessage: "Tee2dWhy Tells you to Authenticate",
+            provider: "walletconnect"
+        })
+            .then(function (user) {
+                console.log("logged in user:", user);
+                document.getElementById("btn-login").innerHTML = (user.get("ethAddress"));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
+
+
+// Footer's Year
+const year = new Date().getFullYear()
+
+document.getElementById("year").innerHTML = year
